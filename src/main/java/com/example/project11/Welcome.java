075dd32a280647df6471d46459460fbd5eb2b3d9
navathadev <navathadeveloper@gmail.com>
2025-07-18 19:39:21 +0530

    2nd commit

--- src/main/java/com/example/project11/Welcome.java
+++ src/main/java/com/example/project11/Welcome.java
@@ -1,11 +1,18 @@
 package com.example.project11;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Welcome {
+    @GetMapping("/welcome")
     public String welcomehome(){
         String msg="WELCOME NAVATHA !!!!";
+        return msg;
+    }
+    @GetMapping("/greet")
+    public String greet(){
+        String msg="Hello good morning";
         return msg;
     }
 }
