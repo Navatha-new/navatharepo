9828a30507112b05e567cc6f18695f2436951a5c
navathadev <navathadeveloper@gmail.com>
2025-07-18 18:33:48 +0530

    controller 1st commit

--- src/main/java/com/example/project11/Welcome.java
+++ src/main/java/com/example/project11/Welcome.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Welcome {
     public String welcomehome(){
-        String msg="WELCOME NAVATHA!!!!";
+        String msg="WELCOME NAVATHA garu!!!!";
         return msg;
     }
 }
